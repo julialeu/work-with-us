058e234d8c1d34cb570c856c4b2fbb445826bcc5
Julia Leuenberger <leuenberger2101@gmail.com>
2022-04-02 12:28:51 +0200

    Add Get Job Vacancy UseCase Test

---
+++ tests/Unit/Src/Auth/Application/GetJobVacancyUseCaseTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Unit\Src\Auth\Application;
+
+use Tests\TestCase;
+use WorkWithUs\Auth\Application\GetJobVacancyUseCase;
+use WorkWithUs\Auth\Domain\Entity\JobVacancy;
+use WorkWithUs\Auth\Infrastructure\Repository\JobVacancyRepository;
+
+class GetJobVacancyUseCaseTest extends TestCase
+{
+    private GetJobVacancyUseCase $sut;
+
+    private JobVacancyRepository $jobVacancyRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->jobVacancyRepository = $this->createMock(JobVacancyRepository::class);
+
+        $this->sut = new GetJobVacancyUseCase($this->jobVacancyRepository);
+    }
+
+    public function test_get_job_vacancy_is_created_correctly(): void
+    {
+        $jobVacancy = (new JobVacancy())
+            ->setId(1)
+            ->setUserId(2)
+            ->setTitle('Trainee front-end')
+            ->setDescription('Trainee front-end')
+            ->setCompany('Cocacola')
+            ->setLocation('Pamplona')
+            ->setModality('on_site')
+            ->setWorkingTime('Jornada completa')
+            ->setExperience('trainee')
+            ->setUrlToken('churro78123')
+            ->setUuid('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee');
+
+        $this->jobVacancyRepository
+            ->expects(self::once())
+            ->method('findByUuid')
+            ->with('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee')
+            ->willReturn($jobVacancy);
+
+        $result = $this->sut->execute('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee');
+
+        $expected = [
+            'id' => 1,
+            'title' => 'Trainee front-end',
+            'description' => 'Trainee front-end',
+            'company' => 'Cocacola',
+            'location' => 'Pamplona',
+            'modality' => 'on_site',
+            'working_time' => 'Jornada completa',
+            'experience' => 'trainee',
+        ];
+
+        $this->assertSame(
+            $expected,
+            $result
+        );
+    }
+
+
+//    public function test_get_job_vacancy_is_created_correctly(): void
+//    {
+//        $jobVacancy = (new JobVacancy())
+//            ->setId(1)
+//            ->setUserId(2)
+//            ->setTitle('Trainee front-end')
+//            ->setDescription('Trainee front-end')
+//            ->setCompany('Cocacola')
+//            ->setLocation('Pamplona')
+//            ->setModality('on_site')
+//            ->setWorkingTime('Jornada completa')
+//            ->setExperience('trainee')
+//            ->setUrlToken('churro78123')
+//            ->setUuid('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee');
+//
+//        $this->jobVacancyRepository
+//            ->expects(self::once())
+//            ->method('findByUuid')
+//            ->with('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee')
+//            ->willReturn($jobVacancy);
+//
+//        $result = $this->sut->execute('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee');
+//
+//        $expected = [
+//            'id' => 1,
+//            'title' => 'Trainee front-end',
+//            'description' => 'Trainee front-end',
+//            'company' => 'Cocacola',
+//            'location' => 'Pamplona',
+//            'modality' => 'on_site',
+//            'working_time' => 'Jornada completa',
+//            'experience' => 'trainee',
+//        ];
+//
+//        $this->assertSame(
+//            $expected,
+//            $result
+//        );
+//    }
+}
