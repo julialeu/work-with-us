aee98006d7bf5d02c64b11c7aaa314a541cafcaf
Julia Leuenberger <leuenberger2101@gmail.com>
2022-04-12 23:10:56 +0200

    Print job vacancy in blade

---
+++ app/Http/Controllers/Site/ShowJobVacancyPageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+
+class ShowJobVacancyPageController
+{
+
+    public function __invoke(
+        Request $request,
+
+    )
+    {
+        die('ooo');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Site\ShowCompanyPageController;
+
+use App\Http\Controllers\Site\ShowJobVacancyPageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,8 +24,7 @@ Route::get('dummy', 'App\Http\Controllers\DummyController@execute');
 
 Route::get('{companySlug}', ShowCompanyPageController::class);
 
-
-
+Route::get('{companySlug}/{urlToken}', ShowJobVacancyPageController::class);
 
 include 'api.php';
 
